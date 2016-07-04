2c36efaeb700a3088cd9250bb184f4634ae6424e
Admir Obralija <admir.obralija@mathematik.uni-stuttgart.de>
2016-07-04 21:04:15 +0200

    Refactoring methods associated with OpenRemoteDatabaseDialog.

--- src/main/java/net/sf/jabref/gui/JabRefFrame.java
+++ src/main/java/net/sf/jabref/gui/JabRefFrame.java
@@ -1164,6 +1164,7 @@ public class JabRefFrame extends JFrame implements OutputPrinter {
         file.add(exportSelected);
         file.addSeparator();
         file.add(openRemoteDatabaseAction);
+        file.add(pullRemoteChanges);
 
         file.addSeparator();
         file.add(databaseProperties);
@@ -1324,7 +1325,6 @@ public class JabRefFrame extends JFrame implements OutputPrinter {
         quality.add(findUnlinkedFiles);
         quality.add(autoLinkFile);
         quality.add(downloadFullText);
-        quality.add(pullRemoteChanges);
         mb.add(quality);
 
         tools.add(newSubDatabaseAction);
